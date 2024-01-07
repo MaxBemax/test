b4cccd52305c725e5904dc140ed82cc704dd31b1
Max <7510332@gmail.com>
2024-01-07 20:53:23 +0200

    cicle

--- js/script.js
+++ js/script.js
@@ -1,33 +1,21 @@
 'use strict';
 
-// const hamburger = true,
-// 	fries = false;
+let num = 50;
 
-// if (hamburger && fries) {
-// 	console.log('I am full');
+// while (num <= 55) {
+// 	console.log(num);
+// 	num++;
 // }
 
-//  console.log((hamburger && fries));
+// do {
+// 	console.log(num);
+// 	num++;
+// }
+// while (num < 55);
 
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-if (hamburger || cola || fries === 3 || nuggets) {
-	console.log('All people satisfied');
-} 
-
-
-// console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets);
-
-// let alexReport, steveReport, danReport = 'done', lisaReport;
-
-// console.log(alexReport || steveReport || danReport || lisaReport);
-
-console.log(NaN || null || !3 || undefined || 5);
-
-console.log(NaN || null && !3 && undefined || 5);
-
-console.log(5 === 5 && 3 > 1 || 5);
-
+for (let i = 1; i < 10; i++) {
+	if (i === 6) {
+		continue;
+	}
+	console.log(i);
+}
