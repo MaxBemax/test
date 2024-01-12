0460fcf5b2e392c953b2b42ad5e3e151bdd7eb05
Max <7510332@gmail.com>
2024-01-12 10:05:49 +0200

    cycle in cycle and mark

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-'use strict';
-
-let num = 50;
-
-// while (num <= 55) {
-// 	console.log(num);
-// 	num++;
+// for (let i = 0; i < 3; i++) {
+// 	console.log(i);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(j);
+// 	}
 // }
 
-// do {
-// 	console.log(num);
-// 	num++;
-// }
-// while (num < 55);
+// *
+// **
+// ***
+// ****
+// *****
+// ******
 
-for (let i = 1; i < 10; i++) {
-	if (i === 6) {
-		continue;
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+	for (let j = 0; j < i; j++) {
+		result += '*';
 	}
-	console.log(i);
+
+	result += '\n';
+}
+
+console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+	console.log(`First level: ${i}`);
+	for (let j = 0; j < 3; j++) {
+		console.log(`Second level: ${j}`);
+		for (let k = 0; k < 3; k++) {
+			if (k == 2) continue first;
+			console.log(`Third level: ${k}`);
+		}
+	}
 }
