cbc761e57922562f1223e824c776502e196725e8
Max <7510332@gmail.com>
2024-02-03 10:35:30 +0200

    circuit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,14 @@
 'use strict';
-let num = 10;
 
-function showFirstMessage(text) {
-	console.log(text);
-	num = 20;
-	console.log(num);
-	return num;
+function makeCounter() {
+	let count = 0;
+
+	return function () {
+		return count++;
+	};
 }
-showFirstMessage('Hello World!');
-
-// function calc(a, b) {
-// 	return (a + b);
-// }
-
-// console.log(calc(4, 3));
-// console.log(calc(6, 3));
-// console.log(calc(8, 7));
-
-function ret() {
-	let num = 50;
-
-
-	return num;
-}
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function () {
-	console.log('Hello');
-};
-logger();
-
-const calc = (a, b) => a + b;
+let counter = makeCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
