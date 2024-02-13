a06dc24ec37578f2d4322d1673f73202f1a3b08a
Max <7510332@gmail.com>
2024-02-13 11:43:34 +0200

    function part 1

--- js/script.js
+++ js/script.js
@@ -1,83 +1,21 @@
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-	numberOfFilms = +prompt('Скільки фльмів вже продивились?', '');
-   
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt('Скільки фльмів вже продивились?', '');
-	} 
-
-}
-start();
-
-
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-
-function rememberMYFilms() {
-	for (let i = 0; i < 2; i++) {
-		const a = prompt('Один із останніх фільмів які продивилися?', ''),
-			b = prompt('Яку оцінку поставите?', '');
+function calculateVolumeAndArea(side) {
+	if (typeof (side) !== 'number' || side < 0 || !Number.isInteger(side)) {
+		console.log('При обчисленні сталася помилка!');
+		return ;
+	}
+	let volume = 0,
+		area = 0;
 	
-		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-			personalMovieDB.movies[a] = b;
-			console.log('done');
-		} else {
-			console.log('error');
-			i--;
-		}
-	}
-}
-rememberMYFilms();
+	volume = side * side * side;
+	area = 6 * (side * side);
 
-function detectPersonalLevel() {
-	if (personalMovieDB.count < 10) {
-		console.log('Watched very few films!');
-	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-		console.log('You classic viewer!');
-	} else if (personalMovieDB.count >= 30) {
-		console.log('You movie fan!!!');
-	} else {
-		console.log('Error!');
-
-	}
+	console.log(`Об'єм куба: ${volume}, площа всієї поверхні: ${area}`);
+	
+	return;
 }
-detectPersonalLevel();
+calculateVolumeAndArea(15);
 
-function showMyDB (hidden) {
-	if (!hidden) {
-		console.log(personalMovieDB);
-	}
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-	for (let i = 1; i <= 3; i++) {
-		const genres = prompt(`Ваш улюблений жанр під номером ${i}`);
-		personalMovieDB.genres[i - 1] = genres;
-	}
-}
-writeYourGenres();	
 
 
