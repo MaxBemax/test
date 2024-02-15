e7070770e44ccf5431fdd66be32d68055a88f6c7
Max <7510332@gmail.com>
2024-02-15 10:36:25 +0200

    function part 2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 'use strict';
 
-function calculateVolumeAndArea(side) {
-	if (typeof (side) !== 'number' || side < 0 || !Number.isInteger(side)) {
-		console.log('При обчисленні сталася помилка!');
-		return ;
+function getCoupeNumber(seatNumber) {
+	if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+		return 'Помилка! Перевірте правельність введення номера місця';
+	} else if (seatNumber === 0 || seatNumber > 36) {
+		return 'Таких місць в цьому вагоні не існує';
 	}
-	let volume = 0,
-		area = 0;
-	
-	volume = side * side * side;
-	area = 6 * (side * side);
-
-	console.log(`Об'єм куба: ${volume}, площа всієї поверхні: ${area}`);
-	
-	return;
+   
+	return Math.ceil(seatNumber / 4);
 }
-calculateVolumeAndArea(15);
+getCoupeNumber(13);
 
 
 
