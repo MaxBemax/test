1a95fcfbce5a402b9e1cafae3fab9a74e3594083
Max <7510332@gmail.com>
2024-02-20 21:54:39 +0200

    function Numbers of Fibonacci

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 'use strict';
-
-function findMaxNumber(a, b, c, d) {
-	if (typeof (a) !== 'number' || typeof (b) !== 'number' || typeof (c) !== 'number' || typeof (d) !== 'number') {
-		return 0;
-	} else {
-		return Math.max(a, b, c, d);
+function fib(pI) {
+	if (typeof (pI) !== 'number' || pI < 0 || !Number.isInteger(pI)) {
+		console.log('');
+		return '';
 	}
+
+	let result = '';
+	let first = 0;
+	let second = 1;
+
+	for (let i = 0; i < pI; i++) {
+		if (i + 1 === pI) {
+			result += `${first}`;
+			// Без пробела в конце
+		} else {
+			result += `${first} `;
+		}
+
+		let third = first + second;
+		first = second;
+		second = third;
+	}
+
+	console.log(result);
+	return result;
 }
-findMaxNumber(12, 5, 6);
+fib(7);
+
 
 
