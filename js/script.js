4d03f792becbb968cc962f94213f7cd848d36779
Max <7510332@gmail.com>
2024-05-11 20:37:49 +0300

    object part 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const list = {
+	nickname: 'meal',
+	potatoes: 150,
+	tomatoes: 50,
+	pork: {
+		meat: 5,
+		fat: 10
+	}, 
+	fruits: {
+		watermelon: 'yellow',
+		bananas: 'green'
+	},
+	makeTest: function () {
+		console.log('Test');
+	}
+};
+console.log(Object.keys(list).length);
+
+const {meat, fat} = list.pork;
+console.log(meat, fat);
+
+console.log(Object.keys(list).length);
+
 const options = {
 	name: 'test',
 	width: 1024,
@@ -10,19 +33,25 @@ const options = {
 	}
 };
 
+console.log(options);
+
+const { border, bg } = options.color;
+console.log(border, bg);
+
+// console.log(Object.keys(options).length);
 // console.log(options['color']['bg']);
 // delete options.width;
 // console.log(options);
-let counter = 0;
-for (let key in options) {
-	if (typeof (options[key]) === 'object') {
-		for (let i in options[key]) {
-			console.log(`Властивість ${i} має значення ${options[key][i]}`);
-		}
-	} else {
-		console.log(`Властивість ${key} має значення ${options[key]}`);
-	}
-	counter++;
-}
+// let counter = 0;
+// for (let key in options) {
+// 	if (typeof (options[key]) === 'object') {
+// 		for (let i in options[key]) {
+// 			console.log(`Властивість ${i} має значення ${options[key][i]}`);
+// 		}
+// 	} else {
+// 		console.log(`Властивість ${key} має значення ${options[key]}`);
+// 	}
+// 	counter++;
+// }
 
-console.log(counter);
+// console.log(counter);
