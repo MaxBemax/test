75661a4554b61fb06fb60df39780318d7264842f
Max <7510332@gmail.com>
2024-05-18 21:50:09 +0300

    transmission by reference and by value

--- js/script.js
+++ js/script.js
@@ -1,31 +1,21 @@
 'use strict';
-// const arr = [5, 28, 9, 16, 12];
 
-// arr.sort((a, b) => a - b);
-// console.log(arr);
+let a = 5,
+	b = a;
 
-// // function compareNum(a, b) {
-// // 	return a - b;
-// // }
+b = b + 5;
 
-// const str = prompt('', '');
-// const products = str.split(', ');
-// console.log(products.join(';'));
+console.log(b);
+console.log(a);
 
-const arrKeys = [32, 44, 25, 34, 12],
-	arrLink = [23, 15, 64, 78];
+const obj = {
+	a: 1,
+	b: 2
+};
 
-const arrEx = arrKeys.concat(arrLink); 
-// arrKeys.sort((a, b) => a - b);
-// for (let value of arrKeys) {
-// 	console.log(value);
-// }
+const copy = obj;
 
-// arrKeys.reverse();
-console.log(arrEx);
+copy.a = 10;
 
-const arr = [32, 44, 25, 34, 12];
-
-arr.splice(2, 2, 88, 66);
-
-console.log(arr);
+console.log(copy);
+console.log(obj);
