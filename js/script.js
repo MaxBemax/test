38024afbb023f0546c11dca6a4f92ada4372bbd8
Max <7510332@gmail.com>
2024-06-08 21:08:41 +0300

    tasks with arrey part2

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
 'use strict';
 
-const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+// const someString = 'This is some strange string';
 
-function showFamily(arr) {
+//  function reverse(str) {
+// 	if (typeof (str) !== 'string') {
+//  		return 'Ошибка!';
+//  	}
+//  	return str.split('').reverse().join('');
+//  }
+//  console.log(reverse(someString));
+
+
+
+// function reverse(str) {
+// 	let newStr = '';
+// 	for (let i = str.length - 1; i >= 0; i--) {
+// 		newStr += str[i];
+// 	}
+// 	return newStr;
+// }
+// console.log(reverse(someString));
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
 	let str = '';
+	arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+
+	// arr.forEach(function (curr, i) {
+	// 	// if (curr !== missingCurr) {
+	// 	// 	str += `${curr}\n`;
+	// 	// }
+
+		
+	// });
 	
-	arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
-
-	arr.forEach(member => {
-		str += `${member} `;
-	});
-
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === missingCurr) {
+			continue;
+		}
+		str += `${arr[i]}\n`;
+	}
 	return str;
 }
-
-console.log(showFamily(family));
-
-const age = 26;
-const beverage = age >= 21 ? 'Пиво' : 'Сік';
-console.log(beverage);
-
-const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
-
-function standardizeStrings(arr) {
-	arr.forEach(city => {
-		console.log(city.toLowerCase());
-	});
-}
-standardizeStrings(favoriteCities);
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies]), 'CNY');
