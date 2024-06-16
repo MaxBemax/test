2b26f0a5cff1016f9a44876124dfc4f11e7ea67f
Max <7510332@gmail.com>
2024-06-16 10:39:38 +0300

    object- oriented programming

--- js/script.js
+++ js/script.js
@@ -1,47 +1,27 @@
 'use strict';
 
-// const someString = 'This is some strange string';
+// let str = 'some';
+// let strObj = new String(str);
 
-//  function reverse(str) {
-// 	if (typeof (str) !== 'string') {
-//  		return 'Ошибка!';
-//  	}
-//  	return str.split('').reverse().join('');
-//  }
-//  console.log(reverse(someString));
+// // console.log(typeof(str));
+// // console.log(typeof(strObj));
 
+// console.dir([1, 2, 3]);
 
-
-// function reverse(str) {
-// 	let newStr = '';
-// 	for (let i = str.length - 1; i >= 0; i--) {
-// 		newStr += str[i];
-// 	}
-// 	return newStr;
-// }
-// console.log(reverse(someString));
-
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-
-function availableCurr(arr, missingCurr) {
-	let str = '';
-	arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
-
-	// arr.forEach(function (curr, i) {
-	// 	// if (curr !== missingCurr) {
-	// 	// 	str += `${curr}\n`;
-	// 	// }
-
-		
-	// });
-	
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === missingCurr) {
-			continue;
-		}
-		str += `${arr[i]}\n`;
+const soldier = {
+	health: 400,
+	armor: 100,
+	sayHello: function () {
+		console.log('Hello');
 	}
-	return str;
-}
-console.log(availableCurr([...baseCurrencies, ...additionalCurrencies]), 'CNY');
+};
+
+const jonh = {
+	health: 100
+};
+
+// jonh._proto_ = soldier;
+
+Object.setPrototypeOf(jonh, soldier);
+
+jonh.sayHello();
