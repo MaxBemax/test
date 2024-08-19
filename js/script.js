672136a32ede2cf7ed8c1c9bce513f5a46a88b56
Max <7510332@gmail.com>
2024-08-19 10:34:14 +0300

    clouser and lexical environmant

--- js/script.js
+++ js/script.js
@@ -1,40 +1,13 @@
 'use strict';
 
-function hello() {
-	console.log('Hello world!');
+let number = 5;debugger
+
+function logNumber() {
+	let number = 4;debugger
+	console.log(number);
 }
-hello();
 
-function hi() {
-	console.log('Say hi!');
-}
-hi();
+number = 6;
 
-const arr = [1, 14, 4, 30, 54],
-	sorted = arr.sort(compareNum);
+logNumber();debugger
 
-function compareNum(a, b) {
-	return a - b;
-}
-console.log(sorted);
-	
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam' ];
-
-function sortStudentsByGroups(arr) {
-	arr.sort();
-	const a = [], b = [], c = [], rest = [];
-
-	for (let i = 0; i < arr.length; i++) {
-		if (i < 3) {
-			a.push(arr[i]);
-		} else if (i < 6) {
-			b.push(arr[i]);
-		} else if (i < 9) {
-			c.push(arr[i]);
-		} else {
-			rest.push(arr[i]);
-		}
-	}
-	return [a, b, c, `Залишок студентів: ${rest.length === 0 ? '-' : rest.join(', ')}`];
-}
-console.log(sortStudentsByGroups(students));
