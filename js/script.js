2c3191b5f0851dfd520f5a7f1810fdb22534285a
Max <7510332@gmail.com>
2024-08-23 20:42:52 +0300

    console tasks

--- js/script.js
+++ js/script.js
@@ -1,13 +1,73 @@
 'use strict';
 
-let number = 5;debugger
+// let number = 5; debugger;
 
-function logNumber() {
-	let number = 4;debugger
-	console.log(number);
-}
+// function logNumber() {
+// 	let number = 4; debugger;
+// 	console.log(number);
+// }
 
-number = 6;
+// number = 6;
 
-logNumber();debugger
+// logNumber(); debugger;
 
+// function createCounter() {
+// 	let counter = 0;
+
+// 	const myFunction = function () {
+// 		counter = counter + 1;
+// 		return counter;
+// 	};
+
+// 	return myFunction;
+// }
+
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+
+// console.log(c1, c2, c3);
+
+// console.log(typeof (NaN));
+
+// const getSum = function (a, b) {
+// 	return a + b;
+// };
+// const result = getSum(5, 6);
+// console.log(result);
+
+// console.log(0 || 1);
+
+// console.log(0 && 1);
+
+// console.log('a' > 'b');
+
+// console.log(0 || NaN || false || null);
+
+// let x = 5; alert(++x);
+
+// console.log([] + false - null + true);
+
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// console.log(typeof([] + 1 + 2));
+
+// alert('1'[0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// console.log(!! (1 && 2) === (1 && 2));
+
+// alert(null || 2 && 3 || 4);
+
+// const a = [1, 2, 3],
+//    b = [1, 2, 3];
+   
+// console.log(a == b);
+
+// console.log(+'Infinity');
+
+console.log('Їжак' > 'яблуко');
